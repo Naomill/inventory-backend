7fff801e6a964e20d955fbb3b43bedcd686a6ad6
Naomill <saraleedrschool@gmail.com>
2024-12-06 13:39:47 +0700

    Change to use Railway mysql service

--- db.js
+++ db.js
@@ -1,17 +1,7 @@
 const mysql = require('mysql2');
-require('dotenv').config();
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
-
-module.exports = pool.promise();
-
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -19,9 +9,8 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) {
-        console.error('Database connection failed:', err.stack);
+        console.error('Database connection failed:', err.message);
         return;
     }
-    console.log('Connected to database.');
-    connection.end();
+    console.log('Connected to the database.');
 });
